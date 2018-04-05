8b754df213b766cfe938b1ad1c6b311aaeee597c
Mat Allen <mallen@redhat.com>
2018-04-05 00:24:54 -0500

    xx

---
+++ src/main/java/com/redhat/sso/ninja/utils/FilePermissions.java
@@ -0,0 +1,19 @@
+package com.redhat.sso.ninja.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.PosixFilePermission;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FilePermissions{
+
+  public static void set(File file, PosixFilePermission... perms) throws IOException{
+    Set<PosixFilePermission> perms2 = new HashSet<PosixFilePermission>();
+    for(PosixFilePermission p:perms)
+      perms2.add(p);
+    Files.setPosixFilePermissions(file.toPath(), perms2);
+
+  }
+}
