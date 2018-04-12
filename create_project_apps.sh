beb354a29e2ece373c7c4f90c48500b81b393fdb
Mat Allen <mallen@redhat.com>
2018-04-12 14:12:25 -0500

    tried to mitigate the race condition for manual setup steps

---
+++ create_project_apps.sh
@@ -0,0 +1,13 @@
+
+# CREATE DEPLOYMENT APP
+oc process --param-file=applier/params/community-ninja-board-deployment -f applier/templates/community-ninja-board-deployment.yml | oc apply -f-
+#deploymentconfig "community-ninja-board" created
+#route "community-ninja-board" created
+#service "community-ninja-board" created
+
+# CREATE BUILD APP
+oc process --param-file=applier/params/community-ninja-board-build -f applier/templates/community-ninja-board-build.yml | oc apply -f-
+#imagestream "community-ninja-board" created
+#imagestream "community-ninja-board-base" created
+#buildconfig "community-ninja-board-base" created
+#buildconfig "community-ninja-board" created
