c154c6b739fbe0df8a1b96e9a3273e5b7deea87c
Mat Allen <mallen@redhat.com>
2018-11-08 13:44:08 -0600

    added ability to pass params from scripts to dashboard for event logging purposes, updated LnF a bit, and prevented the last date being updated on script failure so it can re-run next time automatically

--- src/main/java/com/redhat/sso/ninja/PointsAdder.java
+++ src/main/java/com/redhat/sso/ninja/PointsAdder.java
@@ -1,6 +1,8 @@
 package com.redhat.sso.ninja;
 
+import java.util.Map;
+
 public interface PointsAdder{
-  public void addPoints(String username, String pool, Integer increment, String sourceEntityId);
+  public void addPoints(String username, String pool, Integer increment, Map<String, String> params);
 //  public void execute(String copName, Integer daysFromLastRun);
 }
