622064764fbeb93925a35b9aef39e3163e13be90
Derek Foster <derekfoster94@gmail.com>
2020-05-01 15:00:10 -0700

    react project set up + patternfly navbar

---
+++ ninja-react/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import '@patternfly/react-core/dist/styles/base.css';
+import NavBar from './components/NavBar';
+
+function App() {
+  return (
+
+    <NavBar />
+
+
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+  );
+}
+
+export default App;
