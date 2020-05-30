c5a644e4c72aadbf885def567b9b68c63ef618d7
Derek Foster <derekfoster94@gmail.com>
2020-05-29 23:35:37 -0700

    refactored nav so the expandables close when clicking a link

--- ninja-react/src/AppRoutes.jsx
+++ ninja-react/src/AppRoutes.jsx
@@ -7,6 +7,63 @@ import HomeSection from "./components/Home";
 /**
  * @author fostimus
  */
+
+ export const ninjaRoutes = [
+   {
+     id: 1,
+     routeName: "Scorecards",
+     routePath: "scorecards"
+   },
+   {
+     id: 2,
+     routeName: "Leaderboard",
+     routePath: "leaderboard"
+   },
+   {
+     id: 3,
+     routeName: "Events",
+     routePath: "events"
+   },
+   {
+     id: 4,
+     routeName: "Tasks",
+     routePath: "tasks"
+   },
+   {
+     id: 5,
+     routeName: "Registration Form",
+     routePath: "registration-form"
+   },
+   {
+     id: 6,
+     routeName: "Responses Spreadsheet",
+     routePath: "responses-spreadsheets"
+   },
+   {
+     id: 7,
+     routeName: "Support - Trello Query",
+     routePath: "support/trello-query"
+   },
+   {
+     id: 8,
+     routeName: "Support - Trello Reconciliation",
+     routePath: "support/trello-reconciliation"
+   }
+ ];
+
+ export const adminRoutes = [
+   {
+     id: 1,
+     routeName: "Config",
+     routePath: "config"
+   },
+   {
+     id: 2,
+     routeName: "Database",
+     routePath: "database"
+   }
+ ];
+
 const AppRoutes = () => {
   return (
     <Fragment>
