7a63ceeb4e96dbd2362211d21bac6a6fbf2c62c9
Derek Foster <defoster22@ucla.edu>
2020-06-08 20:01:22 -0700

    Issue 91 support routing (#94)
    * introduced a hook, and started swithcing to function component
    
    * pushed api model to npm
    
    * converted NavBar to function component, AppRoutes updates (prepend all with /, made the arrays the source of truth)
    
    * remote module import works

--- react/ninja-react/src/AppRoutes.jsx
+++ react/ninja-react/src/AppRoutes.jsx
@@ -9,60 +9,66 @@ import HomeSection from "./components/Home";
  * @author fostimus
  */
 
-//TODO: minimize places to update, use ninjaRoutes and appRoutes values directly in React Router
 export const ninjaRoutes = [
   {
     id: 1,
     routeName: "Scorecards",
-    routePath: "scorecards"
+    routePath: "/scorecards",
+    component: ScorecardSection
   },
   {
     id: 2,
     routeName: "Leaderboard",
-    routePath: "leaderboard"
+    routePath: "/leaderboard"
   },
   {
     id: 3,
     routeName: "Events",
-    routePath: "events"
+    routePath: "/events"
   },
   {
     id: 4,
     routeName: "Tasks",
-    routePath: "tasks"
+    routePath: "/tasks"
   },
   {
     id: 5,
     routeName: "Registration Form",
-    routePath: "registration-form"
+    routePath: "/registration-form",
+    component: FormSection
   },
   {
     id: 6,
     routeName: "Responses Spreadsheet",
-    routePath: "responses-spreadsheets"
+    routePath: "/responses-spreadsheets"
   },
   {
     id: 7,
     routeName: "Support - Trello Query",
-    routePath: "support/trello-query"
+    routePath: "/support/trello-query"
   },
   {
     id: 8,
     routeName: "Support - Trello Reconciliation",
-    routePath: "support/trello-reconciliation"
+    routePath: "/support/trello-reconciliation"
   }
 ];
 
+//TODO: leverage component field in <Switch> component
 export const adminRoutes = [
   {
     id: 1,
     routeName: "Config",
-    routePath: "config"
+    routePath: "/config",
+    component: AdminSection,
+    adminPage: "Config"
   },
   {
     id: 2,
     routeName: "Database",
-    routePath: "database"
+    routePath: "/database",
+    component: AdminSection,
+    adminPage: "Database"
   }
 ];
 
@@ -70,26 +76,24 @@ const AppRoutes = () => {
   return (
     <Fragment>
       <Switch>
-        <Route
-          key="user-registration-page"
-          path="/registration-form"
-          render={() => <FormSection />}
-        />
-        <Route
-          key="config-page"
-          path="/config"
-          render={() => <AdminSection adminPage="Config" />}
-        />
-        <Route
-          key="database-page"
-          path="/database"
-          render={() => <AdminSection adminPage="Database" />}
-        />
-        <Route
-          key="scorecards-page"
-          path="/scorecards"
-          render={() => <ScorecardSection />}
-        />
+        {ninjaRoutes.map(route => (
+          <Route
+            key={route.routeName}
+            path={route.routePath}
+            component={route.component}
+          />
+        ))}
+
+        {adminRoutes.map(route => (
+          <Route
+            key={route.routeName}
+            path={route.routePath}
+            render={props => (
+              <AdminSection {...props} adminPage={route.adminPage} />
+            )}
+          />
+        ))}
+
         <Route key="home-page" exact path="/" render={() => <HomeSection />} />
       </Switch>
     </Fragment>
