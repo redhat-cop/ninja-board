e9914316802e7542287798a306980e530b7ef7fd
Derek Foster <derekfoster94@gmail.com>
2020-06-09 12:01:09 -0700

    logic for dynamically handling data from API

--- react/ninja-react/src/components/Scorecard.jsx
+++ react/ninja-react/src/components/Scorecard.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Table,
   TableHeader,
@@ -8,85 +8,114 @@ import {
 } from "@patternfly/react-table";
 import { PageSection } from "@patternfly/react-core";
 import API from "../config/ServerUrls";
-import Scorecard from "giveback_ninja";
+import {
+  tempScorecardData,
+  scorecardExample
+} from "../config/TempScorecardData";
 
 /**
  * @author fostimus
  */
-export default function ScorecardSection() {
+const ScorecardSection = () => {
   return (
     <PageSection>
       <SortableTable />
     </PageSection>
   );
-}
+};
 
-class SortableTable extends React.Component {
+export default ScorecardSection;
 
+export const SortableTable = props => {
+  // treat each object liket a dictionary, get the "keys" and use them as the column names
+  // after going through entire payload, sort columns alphabetically
+  //
+  // use the values as the row data, each row being ONE scorecard. populate each position in the row based on the key from JSON object.
 
-  constructor(props) {
-    super(props);
-    //TODO: populate rows and columns dynamically based on serverData
-    this.state = {
-      serverData: {},
-      columns: [
-        { title: "Repositories", transforms: [sortable] },
-        "Branches",
-        { title: "Pull requests", transforms: [sortable] },
-        "Workspaces",
-        "Last Commit"
-      ],
-      rows: [
-        ["one", "two", "a", "four", "five"],
-        ["a", "two", "k", "four", "five"],
-        ["p", "two", "b", "four", "five"]
-      ],
-      sortBy: {}
-    };
-    this.onSort = this.onSort.bind(this);
+  let initialColumns = [];
 
-    this.getScorecardData = event => {
-      event.preventDefault();
+  getColumnHeadersFromObject(scorecardExample, initialColumns);
 
-      //TODO: ensure this actually syncs to the backend API
-      API.get(`/scorecard`).then(res => {
-        console.log(res);
-        console.log(res.data);
-        this.setState({
-          serverData: res.data
-        });
-      });
-    };
-  }
+  const createObjectRow = (object, row) => {
+    Object.keys(object).forEach(key => {
+      const position = initialColumns.indexOf(key);
+      if (position == -1) {
+        createObjectRow(object[key], row);
+      } else {
+        row.splice(position, 1, object[key]);
+      }
+    });
+  };
 
-  onSort(_event, index, direction) {
-    const sortedRows = this.state.rows.sort((a, b) =>
+  const initialRows = tempScorecardData.map(scorecard => {
+    let row = [];
+    for (var i = 0; i < initialColumns.length; i++) {
+      row.push(0)
+    }
+    createObjectRow(scorecard, row);
+    return row;
+  });
+
+  // create collection of columns with sortable functionality
+  const sortableColumns = initialColumns.map(header => {
+    const sortableColumn = { title: header, transforms: [sortable] };
+    return sortableColumn;
+  });
+
+  const [serverData, setServerData] = useState({});
+  const [columns, setColumns] = useState(sortableColumns);
+  const [rows, setRows] = useState(initialRows);
+  const [sortBy, setSortBy] = useState({});
+
+  const onSort = (_event, index, direction) => {
+    const sortedRows = rows.sort((a, b) =>
       a[index] < b[index] ? -1 : a[index] > b[index] ? 1 : 0
     );
-    this.setState({
-      sortBy: {
-        index,
-        direction
-      },
-      rows:
-        direction === SortByDirection.asc ? sortedRows : sortedRows.reverse()
+    setSortBy({
+      index,
+      direction
     });
-  }
-
-  render() {
-    const { columns, rows, sortBy } = this.state;
-
-    return (
-      <Table
-        aria-label="Sortable Table"
-        sortBy={sortBy}
-        onSort={this.onSort}
-        cells={columns}
-        rows={rows}
-      >
-        <TableHeader />
-        <TableBody />
-      </Table>
+    setRows(
+      direction === SortByDirection.asc ? sortedRows : sortedRows.reverse()
     );
-  }
-}
+  };
+
+  useEffect(() => {
+    //TODO: ensure this actually syncs to the backend API
+    API.get(`/scorecard`).then(res => {});
+  }, [serverData]);
+
+  return (
+    <Table
+      aria-label="Sortable Table"
+      sortBy={sortBy}
+      onSort={onSort}
+      cells={columns}
+      rows={rows}
+    >
+      <TableHeader />
+      <TableBody />
+    </Table>
+  );
+};
+
+const isObject = value => {
+  return value && typeof value === "object" && value.constructor === Object;
+};
+
+//recursive method to get all column headers into one array
+const getColumnHeadersFromObject = (object, columnHeaders, rows) => {
+  Object.keys(object).forEach(key => {
+    // set up column headers
+    if (!columnHeaders.includes(key)) {
+      if (!isObject(object[key])) {
+        columnHeaders.push(key);
+      } else {
+        getColumnHeadersFromObject(object[key], columnHeaders);
+      }
+    }
+
+    let row = [];
+    row.push();
+  });
+};
--- react/ninja-react/src/config/TempScorecardData.js
+++ react/ninja-react/src/config/TempScorecardData.js
@@ -1,10 +1,10 @@
-export const tempScorecardData = [
-  {
-    id: 1,
-    username: "super_ninja",
-    total: 248,
-    beltColor: "black",
-    pointsToNextLevel: 0,
+export const scorecardExample = {
+  username: "super_ninja",
+  level: "black",
+  score: 248,
+  pointsToNextLevel: 0,
+  nextLevel: "",
+  pointMap: {
     githubReviewedPullRequests: 24,
     githubClosedIssues: 0,
     servicesSupportMergeRequests: 0,
@@ -15,5 +15,41 @@ export const tempScorecardData = [
     trelloCardsClosed: 23,
     thoughtLeadershipCardsClosed: 1,
     servicesSupportCardsClosed: 10
+  }
+};
+
+export const tempScorecardData = [
+  {
+    username: "super_ninja",
+    level: "black",
+    score: 248,
+    pointsToNextLevel: 0,
+    nextLevel: "",
+    details: {
+      githubReviewedPullRequests: 24,
+      githubPullRequests: 34,
+      gitlabMergeRequests: 43,
+      trelloCardsClosed: 23,
+      thoughtLeadershipCardsClosed: 1,
+      servicesSupportCardsClosed: 10
+    }
+  },
+  {
+    username: "super_ninja",
+    level: "black",
+    score: 248,
+    pointsToNextLevel: 0,
+    nextLevel: "",
+    details: {
+      githubReviewedPullRequests: 4,
+      githubClosedIssues: 32,
+      servicesSupportMergeRequests: 98,
+      servicesSupportClosedIssues: 2,
+      servicesSupportReviewedMergeRequests: 4,
+      githubPullRequests: 3,
+      gitlabMergeRequests: 3,
+      trelloCardsClosed: 10,
+      thoughtLeadershipCardsClosed: 15
+    }
   }
 ];
