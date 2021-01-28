f5d3cf9d64f82dad47b33b1131f3e50d72e02d1f
Mat Allen <mallen@redhat.com>
2021-01-28 08:36:45 -0600

    removed commented working code

--- src/main/java/com/redhat/sso/ninja/ScriptRunner.java
+++ src/main/java/com/redhat/sso/ninja/ScriptRunner.java
@@ -13,47 +13,6 @@ import org.apache.log4j.Logger;
 import com.google.common.base.Joiner;
 
 public class ScriptRunner{
-//  private static final Logger log=Logger.getLogger(ScriptRunner.class);
-  
-//	public static LineParser defaultLineParser=new LineParser(){
-//		@Override public void parse(String line){
-//			System.out.println(line);
-//		}};
-//	
-//	public interface LineParser{
-//		public void parse(String line);
-//	}
-	
-	
-	public static void main(String[] asd) throws IOException, InterruptedException{
-		File scriptFolder=new File("/home/mallen/temp/ninja4/ninja-board/target/scripts/Trello.ThoughtLeadership/v1.4");
-		new ScriptRunner().run(scriptFolder, "/home/mallen/temp/ninja4/ninja-board/target/scripts/Trello.ThoughtLeadership/v1.4/trello-stats.py -s 2021-01-12 -o servicesmarketing3");
-	}
-	
-//	public int run(File workingFolder, String command, LineParser lineParser){
-//		boolean isWindows=false;
-//		try{
-//			ProcessBuilder pBuilder=new ProcessBuilder(isWindows?"cmd.exe":"/bin/sh").directory(workingFolder).command(command.split(" "));
-//			pBuilder.redirectErrorStream(true);
-//			Process process=pBuilder.start();
-//			String buf;
-//			BufferedReader reader=new BufferedReader(new InputStreamReader(process.getInputStream()));
-//			while ((buf=reader.readLine())!=null) {
-////				log.debug(buf);
-//				lineParser.parse(buf);
-//			}
-//			process.waitFor(10, TimeUnit.SECONDS);
-////			System.out.println(process.exitValue());
-////			System.out.println("==================================");
-//			return process.exitValue();
-//		}catch (IOException e){
-//			e.printStackTrace();
-//		}catch (InterruptedException e1){
-//			e1.printStackTrace();
-//		}
-//		return 1;
-//	}
-	
 	
 	public class ProcessResult{
 		private List<String> lines=new ArrayList<String>();
@@ -82,32 +41,5 @@ public class ScriptRunner{
 		return result;
 	}
 	
-//	public interface ErrorHandler{
-//		public void onError(int exitValue, String lines);
-//	}
-//	public ProcessResult run2(File workingFolder, String command, LineParser lineParser, ErrorHandler errorHandler) throws IOException, InterruptedException{
-//		boolean isWindows=false;
-//		ProcessBuilder pBuilder=new ProcessBuilder(isWindows?"cmd.exe":"/bin/sh").directory(workingFolder).command(command.split(" "));
-//		pBuilder.redirectErrorStream(true);
-//		Process process=pBuilder.start();
-//		String buf;
-//		ProcessResult result=new ProcessResult();
-//		BufferedReader reader=new BufferedReader(new InputStreamReader(process.getInputStream()));
-//		while ((buf=reader.readLine())!=null) {
-////			System.out.println(buf);
-//			result.lines.add(buf);
-//		}
-//		process.waitFor(10, TimeUnit.SECONDS);
-//		result.exitValue=process.exitValue();
-//		
-//		if (0==result.exitValue){
-//			for (String line:result.lines())
-//				lineParser.parse(line);
-//		}else{
-//			errorHandler.onError(result.exitValue, Joiner.on("\n").join(result.lines()));
-//		}
-//		
-//		return result;
-//	}
 	
 }
