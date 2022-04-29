e9fb1e683d182844749f46b3ed788a3eadcbcbb9
Mat Allen <mallen@redhat.com>
2022-04-29 14:39:35 -0500

    added manager field to user event reporting for line managers to see reports

---
+++ src/test/java/com/redhat/sso/ninja/controllers/EventsControllerTest.java
@@ -0,0 +1,19 @@
+package com.redhat.sso.ninja.controllers;
+
+import org.junit.Test;
+
+import com.redhat.sso.ninja.utils.MapBuilder;
+
+public class EventsControllerTest{
+
+	@Test
+	public void testGetEventsV2_30DaysOld() throws Exception{
+
+  	long s=System.currentTimeMillis();
+		new EventsController().getEventsV2(new MapBuilder<String,String>().put("daysOld","30").build());
+//  	System.out.println(
+			//new ExportController().exportEvents(null, "csv")
+//  	);
+  	System.out.println("took "+(System.currentTimeMillis()-s+"ms"));
+	}
+}
